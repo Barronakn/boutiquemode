185fc484cace3b4736a3646370eb2cb373842cef
Barron <barronakn@gmail.com>
2023-04-25 15:54:27 +0100

    Mise en ligne

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import Navigation from "../components/Navigation";
 import Boutique from "../components/Boutique";
 import Footer from "../components/Footer";
+import womanPic from "/dist/womanPic.png";
+import hommePic from "/dist/hommePic.png";
 
 const Home = () => {
   return (
@@ -9,9 +11,9 @@ const Home = () => {
         <Navigation />
         <div className="homeContainer">
           <div className="homeImg">
-            <img src="/dist/womanPic.png" alt="womanPic" />
+            <img src={womanPic} alt="womanPic" />
             <h1 className="homeTitle">Welcome to Akn Shop</h1>
-            <img src="/dist/hommePic.png" alt="hommePic" />
+            <img src={hommePic} alt="hommePic" />
           </div>
         </div>
       </div>
